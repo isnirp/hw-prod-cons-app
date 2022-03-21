dc16dafb091957f7b2f8d69db44c5a2e9103461f
isnirp <pramankwah@gmail.com>
2022-03-21 15:33:22 +0100

    test prod consumer

--- test_dummy_app.py
+++ test_dummy_app.py
@@ -1,5 +1,7 @@
 import unittest
+from concurrent import futures
 from dummy_app import BoundedQueue
+import dummy_app
 
 class TestDummyApp(unittest.TestCase):
 
@@ -16,6 +18,12 @@ class TestDummyApp(unittest.TestCase):
         result = self.queue_.take()
 
         self.assertEqual(result, "Prince")
+
+    def test_producer_consumer(self):
+        queue_2 = BoundedQueue(queue_capacity=2)
+        with futures.ThreadPoolExecutor() as executor:
+            executor.submit(dummy_app.hw_producer, "Rita", queue_2)
+            executor.submit(dummy_app.hw_consumer, queue_2)
 
 if __name__ == '__main__':
     unittest.main()
