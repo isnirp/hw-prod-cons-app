9ae9048b60dcc997b1e1fe89d8276179d0f53dd4
isnirp <pramankwah@gmail.com>
2022-03-21 12:28:40 +0100

    producer and consumer threads

--- dummy_app.py
+++ dummy_app.py
@@ -18,7 +18,7 @@ class BoundedQueue:
         try:
             while len(self.queue_) == self.max_size:
                 self.condition_.wait()
-                
+
             self.queue_.append(name)
             print(f'added {name}')
             # self.log.debug(f'added {name}')
@@ -42,6 +42,26 @@ class BoundedQueue:
     def len_(self) -> int:
         return len(self.queue_)
 
+def hw_producer(name: str, queue_:BoundedQueue):
+    queue_.add(name)
+
+def hw_consumer(queue_:BoundedQueue):
+    name = queue_.take()
+    print(f'hello {name}')
+
+def main():
+    queue_ = BoundedQueue(queue_capacity=1)
+    name = "Rita"
+    thread_producer = Thread(target=hw_producer, args=(name, queue_))
+    thread_consumer = Thread(target=hw_consumer, args=(queue_,))
+
+    thread_producer.start()
+    thread_consumer.start()
+
+    thread_producer.join()
+    thread_consumer.join()
+
+    print(f'goodbye {name}')
 
 if __name__ == '__main__':
-    pass
+    main()
